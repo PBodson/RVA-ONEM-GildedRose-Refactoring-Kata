ee85e8f7f35e80e3ec2de1946b58a4710bf8dcee
Pierre Bodson <pierre.bodson@etnic.be>
2024-02-13 11:18:48 +0100

    Refactoring : move constants name to util

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,10 +2,9 @@ package com.gildedrose;
 
 import com.gildedrose.updateQualityStrategies.*;
 
+import static com.gildedrose.ItemUtil.*;
+
 class GildedRose {
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     Item[] items;
 
     public GildedRose(Item[] items) {
--- Java/src/main/java/com/gildedrose/ItemUtil.java
+++ Java/src/main/java/com/gildedrose/ItemUtil.java
@@ -2,6 +2,10 @@ package com.gildedrose;
 
 public class ItemUtil {
 
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     private ItemUtil() {}
 
     public static void increaseQualityIfPossible(Item item) {
