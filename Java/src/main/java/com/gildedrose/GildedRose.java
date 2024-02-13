ff8081d779aabca35311f5300d3c553b7e0360c3
Pierre Bodson <pierre.bodson@etnic.be>
2024-02-13 10:13:46 +0100

    Refactoring : - Invert negative conditions

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,28 +16,28 @@ class GildedRose {
                 continue;
             }
 
-            if (!AGED_BRIE.equals(item.name)
-                && !BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.equals(item.name)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            if (AGED_BRIE.equals(item.name)
+                || BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.equals(item.name)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                    if (BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.equals(item.name)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                        if (BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.equals(item.name)) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
                             }
-                        }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
                             }
                         }
                     }
+                } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
 
@@ -46,17 +46,17 @@ class GildedRose {
 
 
             if (item.sellIn < 0) {
-                if (!AGED_BRIE.equals(item.name)) {
-                    if (!BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.equals(item.name)) {
+                if (AGED_BRIE.equals(item.name)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT.equals(item.name)) {
+                        item.quality = 0;
+                    } else {
                         if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
                     }
                 }
             }
